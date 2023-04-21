3f7619d1392feb454e9af07b3afd8f421cf2400c
Kevin Wang <kevmo314@gmail.com>
2023-04-20 21:09:53 -0400

    fix: preload model

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,16 +1,17 @@
 import { env, InferenceSession } from "onnxruntime-web";
 
+env.wasm.wasmPaths = {
+  "ort-wasm-simd-threaded.wasm": "/ort-wasm-simd-threaded.wasm",
+  "ort-wasm-simd.wasm": "/ort-wasm-simd.wasm",
+  "ort-wasm-threaded.wasm": "/ort-wasm-threaded.wasm",
+  "ort-wasm.wasm": "/ort-wasm.wasm",
+};
+const model = InferenceSession.create(
+  "/interactive_module_quantized_592547_2023_03_19_sam6_long_uncertain.onnx"
+);
+
 window.addEventListener("message", async function (event) {
-  env.wasm.wasmPaths = {
-    "ort-wasm-simd-threaded.wasm": "/ort-wasm-simd-threaded.wasm",
-    "ort-wasm-simd.wasm": "/ort-wasm-simd.wasm",
-    "ort-wasm-threaded.wasm": "/ort-wasm-threaded.wasm",
-    "ort-wasm.wasm": "/ort-wasm.wasm",
-  };
-  const model = await InferenceSession.create(
-    "/interactive_module_quantized_592547_2023_03_19_sam6_long_uncertain.onnx"
-  );
-  const results = await model.run(event.data);
+  const results = await (await model).run(event.data);
 
   event.source?.postMessage(results, "*" as any);
 });
